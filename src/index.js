04568312c828514c82385ba8a5b6ac60dc861340
Gabriel Montagné Láscaris-Comneno <gabriel@rojored.com>
2016-08-07 19:09:52 +0100

    add compare-with

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { batch } from './batch'
+export { compareWith } from './compare-with'
 export { functor } from './functor'
 export { identity } from './identity'
 export { negate } from './negate'
