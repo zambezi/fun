b142647c5b76903ef59d352abc2cfaa7f097942f
Gabriel Montagné <Gabriel.Montagne@icap.com>
2016-09-06 11:01:12 +0100

    Make "replace array contents" return original array.
    And add coverage to it.

---
+++ test/replace-array-contents_test.js
@@ -0,0 +1,20 @@
+import { strictEqual, deepEqual } from 'assert'
+import { replaceArrayContents } from '../src'
+
+describe('replace array contents', () => {
+  it('should change all the elements of an array', () => {
+    const before = [ 1, 2, 3 ]
+        , after = [ 4, 5, 6 ]
+
+    replaceArrayContents(before, after)
+
+    deepEqual(before, after)
+
+  })
+
+  it('should return the original array reference', () => {
+    const a = [ 1, 2, 3 ]
+    strictEqual(replaceArrayContents(a, [ 4, 5, 6 ]), a)
+  })
+
+})
